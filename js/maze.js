adcf0afb0c37da3b8812ef1fe642c1e13a86a72a
ecadet-mum <51096249+ecadet-mum@users.noreply.github.com>
2019-07-09 22:08:24 -0400

    ok

--- js/maze.js
+++ js/maze.js
@@ -1,8 +1,29 @@
 window.onload = function() {
-    document.getElementById("boundary1").onmouseover = overBoundary;
+    $("#boundary1").mouseover(overBoundary);
+    $("#start").click(startClick);
+    $("#end").mouseover(overEnd);
+    $("div#maze div.boundary").mouseover(overBoundary);
+
 };
 
 function overBoundary() {
     // document.getElementById("boundary1").style.backgroundColor = "#ff8888";
     $(".boundary").addClass("youlose");
+    loser = true;
+}
+
+
+function startClick() {
+    loser = false;
+    $(".boundary").removeClass("youlose");
+
+}
+
+function overEnd() {
+    if(loser===true) {
+        alert("Sorry, you lost. :[");
+    } else {
+        $(".boundary").addClass("youwin");
+        alert("You win! :]");
+    }
 }
